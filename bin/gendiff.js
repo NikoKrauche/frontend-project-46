dd30d4bedf7789f48ceb91ce71c6adae20c4f8d6
NikoKrauche <K0lan@ya.ru>
2023-09-10 22:42:13 +0300

    add option -f gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,4 +8,7 @@ program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.');
 
+program
+  .option('-f, --format <type>', 'output format');
+
 program.parse();
