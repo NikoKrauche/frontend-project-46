109bf2e600407298bb928d160d8d8590ba073f8f
NikoKrauche <K0lan@ya.ru>
2023-09-10 20:37:03 +0300

    gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,6 @@ const program = new Command();
 
 program
   .version('1.0.0')
-  .description('Compares two configuration files and shows a difference.')
+  .description('Compares two configuration files and shows a difference.');
 
 program.parse();
