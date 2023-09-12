cd431d42fddb1a38650e75886ce9470e8a96631c
NikoKrauche <K0lan@ya.ru>
2023-09-12 22:49:17 +0300

    update formator.js

--- src/formator.js
+++ src/formator.js
@@ -1,12 +1,18 @@
 const formator = (keys) => {
   const result = keys.map((node) => {
-    if (node.status === 'added') return `+ ${node.key}: ${node.value}`;
-    if (node.status === 'deleted') return `- ${node.key}: ${node.value}`;
-    if (node.status === 'changed') return `- ${node.key}: ${node.value1}\n+ ${node.key}: ${node.value2}`;
-    return `  ${node.key}: ${node.value}`;
-  }).join('\n');
+    if (node.status === 'added') {
+      return `  + ${node.key}: ${node.value}`;
+    }
+    if (node.status === 'deleted') {
+      return `  - ${node.key}: ${node.value}`;
+    }
+    if (node.status === 'changed') {
+      return `  - ${node.key}: ${node.value1}\n  + ${node.key}: ${node.value2}`;
+    }
+    return `    ${node.key}: ${node.value}`;
+  });
 
-  return result;
+  return ['{', ...result, '}'].join('\n');
 };
 
 export default formator;
