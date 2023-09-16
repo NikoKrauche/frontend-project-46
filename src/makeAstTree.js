7989b119e3d045afd3db317986d3198f600f5724
NikoKrauche <K0lan@ya.ru>
2023-09-16 22:26:00 +0300

    modified:   src/makeAstTree.js

--- src/makeAstTree.js
+++ src/makeAstTree.js
@@ -4,11 +4,24 @@ const getDiffTree = (data1, data2) => {
   const sortedKeys = _.sortBy(Object.keys({ ...data1, ...data2 }));
 
   const result = sortedKeys.map((key) => {
-    if (!Object.hasOwn(data1, key)) return { key, value: data2[key], status: 'added' };
-    if (!Object.hasOwn(data2, key)) return { key, value: data1[key], status: 'deleted' };
-    if (data1[key] !== data2[key]) return { key, value1: data1[key], value2: data2[key], status: 'changed' };
-
-    return { key, value: data1[key], status: 'unchanged' };
+    if (!Object.hasOwn(data1, key)) {
+      return {
+        key, value: data2[key], status: 'added',
+      };
+    }
+    if (!Object.hasOwn(data2, key)) {
+      return {
+        key, value: data1[key], status: 'deleted',
+      };
+    }
+    if (data1[key] !== data2[key]) {
+      return {
+        key, value1: data1[key], value2: data2[key], status: 'changed',
+      };
+    }
+    return {
+      key, value: data1[key], status: 'unchanged',
+    };
   });
 
   return result;
