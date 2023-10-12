c5688148d71e98974149c790c366cd9bf716c2dd
NikoKrauche <K0lan@ya.ru>
2023-10-12 20:01:52 +0300

    modified:   src/formatters/index.js 	deleted:    src/formatters/json.js 	modified:   src/index.js

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,14 +1,13 @@
 import makeStylish from './stylish.js';
 import makePlain from './plain.js';
-import makeJson from './json.js';
 
-const formator = (data, format) => {
+const formate = (data, format) => {
   switch (format) {
     case 'stylish': return makeStylish(data);
     case 'plain': return makePlain(data);
-    case 'json': return makeJson(data);
+    case 'json': return JSON.stringify(data);
     default: throw new Error(`Unknown format: ${format}!`);
   }
 };
 
-export default formator;
+export default formate;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import path from 'path';
 import { readFileSync } from 'node:fs';
 import getParsedData from './parser.js';
 import getDiffTree from './makeAstTree.js';
-import formator from './formatters/index.js';
+import formate from './formatters/index.js';
 
 const readFile = (filepath) => readFileSync(path.resolve(filepath));
 const getExtension = (filepath) => path.extname(filepath).slice(1);
@@ -12,7 +12,7 @@ const genDiff = (filepath1, filepath2, format = 'stylish') => {
   const data1 = getData(filepath1);
   const data2 = getData(filepath2);
 
-  return formator(getDiffTree(data1, data2), format);
+  return formate(getDiffTree(data1, data2), format);
 };
 
 export default genDiff;
