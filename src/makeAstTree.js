5c187863677f3d5729437a26ab53fd08959a076e
NikoKrauche <K0lan@ya.ru>
2023-10-24 18:00:03 +0300

    modified:   src/makeAstTree.js

--- src/makeAstTree.js
+++ src/makeAstTree.js
@@ -2,32 +2,26 @@ import _ from 'lodash';
 
 const getDiffTree = (data1, data2) => {
   const sortedKeys = _.sortBy(Object.keys({ ...data1, ...data2 }));
-
   const result = sortedKeys.map((key) => {
-    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
-      const children = getDiffTree(data1[key], data2[key]);
-      return { key, children, status: 'nested' };
+    let children;
+    switch (true) {
+      case (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])):
+        children = getDiffTree(data1[key], data2[key]);
+        return { key, children, status: 'nested' };
+      case (!Object.hasOwn(data1, key)):
+        return { key, value: data2[key], status: 'added' };
+      case (!Object.hasOwn(data2, key)):
+        return { key, value: data1[key], status: 'deleted' };
+      case (!_.isEqual(data1[key], data2[key])):
+        return {
+          key, value1: data1[key], value2: data2[key], status: 'changed',
+        };
+      case (_.isEqual(data1[key], data2[key])):
+        return { key, value: data1[key], status: 'unchanged' };
+      default:
+        throw new Error(`The '${key}' does not fit the condition`);
     }
-    if (!Object.hasOwn(data1, key)) {
-      return {
-        key, value: data2[key], status: 'added',
-      };
-    }
-    if (!Object.hasOwn(data2, key)) {
-      return {
-        key, value: data1[key], status: 'deleted',
-      };
-    }
-    if (!_.isEqual(data1[key], data2[key])) {
-      return {
-        key, value1: data1[key], value2: data2[key], status: 'changed',
-      };
-    }
-    return {
-      key, value: data1[key], status: 'unchanged',
-    };
   });
-
   return result;
 };
 
